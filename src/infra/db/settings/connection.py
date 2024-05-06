b8feb84e9c2f6d53c51b404e5d399980a4ef1bbf
Marquezv <vmarquezv@outlook.com.br>
2024-05-06 19:44:26 -0300

    feat: implementing UsersRepository

---
+++ src/infra/db/settings/connection.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class DBConnectionHandler:
+    def __init__(self) -> None:
+        self.__connection_string = "{}+{}://{}:{}@{}:{}/{}".format(
+            'postgresql',
+            'psycopg2',
+            'postgres',
+            'root',
+            'localhost',
+            '5432',
+            'cw_database'
+        )
+
+        self.__engine = self.__create_database_engine()
+        self.session = None
+
+    def __create_database_engine(self):
+        engine = create_engine(self.__connection_string)
+        return engine
+
+    def get_engine(self):
+        return self.__engine
+
+    def __enter__(self):
+        session_maker = sessionmaker(bind=self.__engine)
+        self.session = session_maker()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
---
+++ src/infra/db/settings/connection_test.py
@@ -0,0 +1,9 @@
+import pytest
+from src.infra.db.settings.connection import DBConnectionHandler
+
+@pytest.mark.skip(reason="sensive test")
+def test_create_database_engine():
+    db_connection_handle = DBConnectionHandler()
+    engine = db_connection_handle.get_engine()
+
+    assert engine is not None
