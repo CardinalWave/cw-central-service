3d96046062d4490458b1aa3cbd5983ba9808470b
Marquezv <vmarquezv@outlook.com.br>
2024-07-07 21:36:22 -0300

    refact: Change login controller

--- src/main/composers/user/user_login_composer.py
+++ src/main/composers/user/user_login_composer.py
@@ -2,11 +2,16 @@ from src.data.use_cases.users.user_authenticator import UserAuthenticator
 from src.infra.db.repositories.users.users_repository import UsersRepository
 from src.data.use_cases.users.user_login import UserLogin
 from src.presentation.controllers.users.user_login_controller import UserLoginController
+from src.data.use_cases.relations.user_status import UserStatus
+from src.main.composers.relations.user_group_composer import user_group_composer
+
 
 def user_login_composer():
     auth = UserAuthenticator()
     repository = UsersRepository()
-    use_case = UserLogin(repository, auth)
+    user_group = user_group_composer()
+    user_status = UserStatus(user_group)
+    use_case = UserLogin(repository, auth, user_status)
     controller = UserLoginController(use_case)
 
     return controller.handle
--- src/tests/data/use_cases/users/user_login_test.py
+++ src/tests/data/use_cases/users/user_login_test.py
@@ -1,10 +1,13 @@
 #pylint: disable=redefined-outer-name
 import pytest
+import uuid
 from src.domain.models.user import User
 from src.domain.models.login import Login
+from src.domain.models.session import Session
 from src.tests.infra.mocks.users_repository import UsersRepositorySpy
 from src.tests.data.mocks.users.user_authenticator import UserAuthenticatorSpy
 from src.data.use_cases.users.user_login import UserLogin
+from src.tests.data.mocks.relations.user_status import UserStatusSpy
 
 @pytest.fixture
 def mock_login():
@@ -15,49 +18,56 @@ def mock_user():
     return User(token="39721cd4-6f50-46c5-9d2a-10f9159b09ee",
                 username='Lua', email='lua@outlook.com')
 
-def test_login(mock_login, mock_user):
-    users_auth = UserAuthenticatorSpy()
-    user_login = UserLogin(UsersRepositorySpy(), users_auth)
+@pytest.fixture
+def mock_session():
+    return Session(session_id=str(uuid.uuid4()), device="esp8266_01")
 
-    response = user_login.login(mock_login)
+def test_login(mock_login, mock_user, mock_session):
+    users_auth = UserAuthenticatorSpy()
+    user_status = UserStatusSpy()
+    user_login = UserLogin(UsersRepositorySpy(), users_auth, user_status)
+    response = user_login.login(mock_login, mock_session)
 
     assert response.get("token") == mock_user.token
     assert response.get("email") == mock_user.email
     assert response.get("username") == mock_user.username
 
 
-def test_login_error_email(mock_login):
+def test_login_error_email(mock_login, mock_session):
     mock_login.email = 'email'
 
     users_auth = UserAuthenticatorSpy()
-    user_login = UserLogin(UsersRepositorySpy(), users_auth)
+    user_status = UserStatusSpy()
+    user_login = UserLogin(UsersRepositorySpy(), users_auth, user_status)
 
     try:
-        user_login.login(mock_login)
+        user_login.login(mock_login, mock_session)
         assert False
     except Exception as exception:
         assert str(exception) == "Email ou senha invalido"
 
-def test_login_error_password(mock_login):
+def test_login_error_password(mock_login, mock_session):
     mock_login.password = "777777"
 
     users_auth = UserAuthenticatorSpy()
-    user_login = UserLogin(UsersRepositorySpy(), users_auth)
+    user_status = UserStatusSpy()
+    user_login = UserLogin(UsersRepositorySpy(), users_auth, user_status)
 
     try:
-        user_login.login(mock_login)
+        user_login.login(mock_login, mock_session)
         assert False
     except Exception as exception:
         assert str(exception) == "Email ou senha invalido"
 
-def test_login_user_loged(mock_login):
+def test_login_user_loged(mock_login, mock_session):
     mock_login.email = 'lua2@outlook.com'
 
     users_auth = UserAuthenticatorSpy()
-    user_login = UserLogin(UsersRepositorySpy(), users_auth)
+    user_status = UserStatusSpy()
+    user_login = UserLogin(UsersRepositorySpy(), users_auth, user_status)
 
     try:
-        user_login.login(mock_login)
+        user_login.login(mock_login, mock_session)
         assert False
     except Exception as exception:
         assert str(exception) == "Usuario logado"
