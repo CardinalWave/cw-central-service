acb35e44e22729a2ebddcb137430d67de1976a53
Marquezv <vmarquezv@outlook.com.br>
2024-08-29 10:04:01 -0300

    refact: Log on user_logout action

--- src/data/use_cases/users/user_logout.py
+++ src/data/use_cases/users/user_logout.py
@@ -3,19 +3,22 @@ from src.domain.use_cases.users.user_logout import UserLogout as UserLogoutInter
 from src.infra.db.interfaces.users_repository import UsersRepositoryInterface
 from src.domain.use_cases.users.user_authenticator import UserAuthenticator as UserAuthInterface
 from src.data.erros.domain_errors import BadRequestError
+from src.main.logs.logs_interface import LogInterface
 
 
 class UserLogout(UserLogoutInterface):
     def __init__(self, users_repository: UsersRepositoryInterface,
-                 user_authenticator: UserAuthInterface) -> None:
+                 user_authenticator: UserAuthInterface, logger: LogInterface) -> None:
 
         self.__users_repository = users_repository
         self.__user_authenticator = user_authenticator
+        self.__logger = logger
 
     def logout(self, user: User) -> str:
         try:
             self.__authentication(user=user)
             status = self.__logout_repo(user)
+            self.__logger.log_session(session=user.token, action="user_logout")
             return status
         except BadRequestError:
             return "failed"
