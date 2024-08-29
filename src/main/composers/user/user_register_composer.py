ba65e1092a1613136a1d94099b81fd7d4211f521
Marquezv <vmarquezv@outlook.com.br>
2024-08-29 10:04:16 -0300

    refact: Log on user_register action

--- src/main/composers/user/user_register_composer.py
+++ src/main/composers/user/user_register_composer.py
@@ -1,11 +1,13 @@
 from src.data.use_cases.users.user_authenticator import UserAuthenticator
 from src.data.use_cases.users.user_register import UserRegister
 from src.presentation.controllers.users.user_register_controller import UserRegisterController
+from src.main.logs.logs import Log
 
 
 def user_register_composer():
     auth = UserAuthenticator()
-    use_case = UserRegister(auth)
-    controller = UserRegisterController(use_case)
+    logger = Log()
+    use_case = UserRegister(auth, logger)
+    controller = UserRegisterController(use_case, logger)
 
     return controller.handle
