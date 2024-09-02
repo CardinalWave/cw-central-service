36026dc4a7dfc93dc924adf5d34d304c6efa2046
Marquezv <vmarquezv@outlook.com.br>
2024-09-02 17:04:49 -0300

    refact: Change connection file

--- src/main/logs/log_handler.py
+++ src/main/logs/log_handler.py
@@ -17,14 +17,12 @@ class SocketHandler:
     def connect(self):
         while True:
             try:
-                ip = Config.CW_LOG_TRACE_IP
-                port = int(Config.CW_LOG_TRACE_PORT)
                 self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.sock.connect((ip, port))
-                print(f"Connected to {ip}:{port}")
+                self.sock.connect((self.host, self.port))
+                print(f"Connected to cw-log-trace[{self.host}:{self.port}]")
                 break
             except (socket.error, ConnectionRefusedError) as e:
-                print(f"Connection failed: {e}. Retrying in 5 seconds...")
+                print(f"Connection failed: {e} - cw-log-trace. Retrying in 5 seconds...")
                 time.sleep(5)
 
     def __del__(self):
@@ -37,7 +35,7 @@ class SocketHandler:
             slen = struct.pack(">L", len(data))
             self.sock.send(slen + data)
         except (socket.error, BrokenPipeError) as e:
-            print(f"Error sending data: {e}. Reconneting...")
+            print(f"Error sending data: {e} -  cw-log-trace. Reconnecting...")
             self.connect()
             self.write(message)
 
